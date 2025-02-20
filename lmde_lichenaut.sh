8500dd5378af229ef5c54451de4554dc00ec55c8
lichenaut <mt0n@tutanota.com>
2025-02-19 19:22:35 -0700

    codium settings fix

--- lmde_lichenaut.sh
+++ lmde_lichenaut.sh
@@ -141,11 +141,11 @@ if [[ "$MODE" == "2" ]]; then
     esac
 
     # Swapfile
-   sudo fallocate -l 4G /swapfile
-   sudo chmod 600 /swapfile
-   sudo mkswap /swapfile
-   echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
-   sudo swapon /swapfile
+    sudo fallocate -l 4G /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
+    sudo swapon /swapfile
 
     # Disable autoconnect to wireless
     nmcli -t -f NAME connection show | while read -r CONN; do
